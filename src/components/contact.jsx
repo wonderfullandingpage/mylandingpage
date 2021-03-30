cef5f6175de17f436437562b0868e60fbd12ed5a
lindelof <phragmen@protonmail.com>
2021-03-30 15:15:25 +0800

    Remove console

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,7 +17,7 @@ export const Contact = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(name, email, message)
+    
     emailjs
       .sendForm(
         'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
