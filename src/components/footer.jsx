e564f0ea40b542be151f59060db2a258818d321a
LiamMarega <liammarega85@gmail.com>
2023-01-19 10:12:39 -0300

    initilize template-phone

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,12 @@
 export const Footer = (props) => {
-  
   return (
     <div>
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
+            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
+            <a href='http://www.liammarega.com' rel='noreferrer' target='_blank'>
+              LiamMarega
             </a>
           </p>
         </div>
