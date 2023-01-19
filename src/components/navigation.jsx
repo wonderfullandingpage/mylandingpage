80e3e3dab6005ab45d906a25f769b81841def998
LiamMarega <liammarega85@gmail.com>
2023-01-19 10:23:44 -0300

    navbar scroll

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,26 @@
+import {useEffect, useState} from "react"
+
 export const Navigation = (props) => {
+  const [navBarBackground, setNavBarBackgorund] = useState(false)
+
+  const changeBackground = () => {
+    if (window.scrollY >= 10) {
+      setNavBarBackgorund(true)
+    } else {
+      setNavBarBackgorund(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground)
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground)
+    }
+  }, [])
+
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+    <nav id='menu' className={`navbar  navbar-fixed-top active-${navBarBackground}`}>
       <div className='container'>
         <div className='navbar-header'>
           <button
@@ -9,21 +29,18 @@ export const Navigation = (props) => {
             data-toggle='collapse'
             data-target='#bs-example-navbar-collapse-1'
           >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
+            {" "}
+            <span className='sr-only'>
+              Toggle navigation
+            </span> <span className='icon-bar'></span> <span className='icon-bar'></span>{" "}
+            <span className='icon-bar'></span>{" "}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
             My Landing Page
-          </a>{' '}
+          </a>{" "}
         </div>
 
-        <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
-        >
+        <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
           <ul className='nav navbar-nav navbar-right'>
             <li>
               <a href='#features' className='page-scroll'>
@@ -50,7 +67,6 @@ export const Navigation = (props) => {
                 Testimonials
               </a>
             </li>
-            
           </ul>
         </div>
       </div>
